d37b5789e62181133922de420b805c02bc92c74b
RevDev22 <92617032+RevDev22@users.noreply.github.com>
2021-10-16 13:22:17 +0700

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,14 @@
+let url = location.href;
+let formdata = $('form').serializeArray();
+let data = {};
+
+$.each(formdata, (i,e) => {
+  data[e.name] = e.value;
+});
+
+$.ajax({
+  url: url,
+  method: 'POST',
+  data: data,
+  success: res => console.log(res);
+});
