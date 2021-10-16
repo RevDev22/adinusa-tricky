b9cc6558f36e57ff6fdb9fdf11fa792928f761fe
RevDev22 <92617032+RevDev22@users.noreply.github.com>
2021-10-16 14:52:32 +0700

    Create pbq.js

---
+++ pbq.js
@@ -0,0 +1,46 @@
+let selects = $('select');
+let simulation_code = $('button:contains("Finish")').attr('onclick').split("'")[1];
+let url = 'https://scoring.adinusa.id/simulation/';
+let question = [];
+
+$.each(selects, (i,e) => {
+  question[i] = {
+    answer: 1,
+    isTrue: true
+  };
+});
+
+function sendAnswer(again){
+  if(again){
+    $.ajax({
+      url: url,
+      method: 'POST',
+      data: {
+        simulation_code: simulation_code,
+        pattern_user: getPattern()
+      },
+      success: res => {
+        if(res != 'true'){
+          $.each(question, (i,e) => {
+            if(res.includes(i)){
+              question[i]['answer'] = question[i]['answer'] * 1 + 1;
+            }
+          });
+          sendAnswer(true);
+        }else{
+          sendAnswer(false);
+        }
+      }
+    });
+  }else{
+    return false;
+  }
+}
+
+function getPattern(){
+  let pattern = '';
+  $.each(question, (i,e) => {
+    pattern += e.answer;
+  });
+  return pattern;
+}
