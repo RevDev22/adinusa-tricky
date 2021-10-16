48cb8141bd8f48eda3681b44d54d2460c205b208
RevDev22 <92617032+RevDev22@users.noreply.github.com>
2021-10-16 11:04:52 +0700

    Update module.js

--- module.js
+++ module.js
@@ -3,4 +3,4 @@ $.each($('.uk-accordion-content[aria-hidden=false] ul li a'), (i,e) => {
   text = text.toLowerCase().trim().replaceAll(' ', '-').replaceAll('\n', '').replaceAll(',','');
   let url = 'https://course.adinusa.id/sections/' + text;
   console.log(text);
-})
+});
