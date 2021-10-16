e6647128787d75bf8342d808c6c8b7373260c93f
RevDev22 <92617032+RevDev22@users.noreply.github.com>
2021-10-16 11:14:55 +0700

    Update module.js

--- module.js
+++ module.js
@@ -1,6 +1,26 @@
-$.each($('.uk-accordion-content[aria-hidden=false] ul li a'), (i,e) => {
+const elm = $('.uk-accordion-content[aria-hidden=false] ul li a');
+let urls = [];
+
+$.each(elm, (i,e) => {
   let text = $(e).text();
   text = text.toLowerCase().trim().replaceAll(' ', '-').replaceAll('\n', '').replaceAll(',','');
   let url = 'https://course.adinusa.id/sections/' + text;
-  console.log(text);
+  if(text.includes('kuis') || text.includes('lab')){
+    return false;
+  }
+  urls[i] = url;
+});
+
+$.each(urls, (i,e) => {
+  $.ajax({
+    url: e,
+    method: 'GET',
+    success: res => {
+      console.log(res);
+    }
+  });
+  
+  if(i + 1 == urls.length){
+    location.href = e;
+  }
 });
